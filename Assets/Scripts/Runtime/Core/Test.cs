891484d374043560b1f182c9b258827d6fafd3a0
Martin Obert <martin@obert.cz>
2023-01-02 12:03:37 +0100

    initial commit

---
+++ Assets/Scripts/Runtime/Core/Test.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Obert.Audio.Runtime.Core
+{
+    public class Test : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
