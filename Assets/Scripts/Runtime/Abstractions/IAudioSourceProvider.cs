468bf163e3fc2d492f0718f5a20e1c6047758072
Martin Obert <30500239+martin-obert@users.noreply.github.com>
2023-04-21 21:40:21 +0200

    Documentation 1 (#22)
    * fix: move documentation

--- Assets/Scripts/Runtime/Abstractions/IAudioSourceProvider.cs
+++ Assets/Scripts/Runtime/Abstractions/IAudioSourceProvider.cs
@@ -2,8 +2,16 @@
 
 namespace Obert.Audio.Runtime.Abstractions
 {
+    /// <summary>
+    /// Provides audio source from the pool. One audio source is associate with single audio clip.
+    /// </summary>
     public interface IAudioSourceProvider : IDisposable
     {
+        /// <summary>
+        /// Provides audio source, that matches tag exactly
+        /// </summary>
+        /// <param name="audioTag">Tag to be matched by <see cref="string.Equals(string)"/></param>
+        /// <returns></returns>
         IAudioSource[] ProvideClipContainingTag(string audioTag);
     }
 }
